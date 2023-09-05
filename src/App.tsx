ccccbd231bf3a8eb3f201834b80e76578d579319
Gowtham kumar <kumargowtham.j@gmail.com>
2023-09-05 08:22:25 +0530

    typo

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div>
       <h1>Under construction...</h1>
-      <a href="https://github.com/gowthamk18">Github</a>
+      <a href="https://github.com/gowthamk17">Github</a>
     </div>
   )
 }
