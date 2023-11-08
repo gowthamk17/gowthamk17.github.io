82f02cba1c8e7e6c4556ad879df2aad1182e7147
Gowtham kumar <kumargowtham.j@gmail.com>
2023-11-08 22:48:07 +0530

    added tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import './App.css'
-
 function App() {
 
   return (
     <div>
-      <h1>Coming soon...</h1>
+      <h1 className="">
+        Coming soon...
+      </h1>
       <a href="https://github.com/gowthamk17">Github</a>
     </div>
   )
