20001c99584e7cd01bdf6dea358caf9e3b9b48e6
Gowtham kumar <kumargowtham.j@gmail.com>
2023-11-08 08:30:57 +0530

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <div>
-      <h1>Under construction...</h1>
+      <h1>Coming soon...</h1>
       <a href="https://github.com/gowthamk17">Github</a>
     </div>
   )
